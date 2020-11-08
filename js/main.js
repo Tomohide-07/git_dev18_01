6fe7bcb028e10b6de8adf2bc74869500d3f023d2
Tomohide-07 <66115858+Tomohide-07@users.noreply.github.com>
2020-11-08 11:43:53 +0900

    HTML&CSS課題ファイル一式

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// jsを記述する際はここに記載していく
+
+$(function(){
+  $('a[href^="#"]').click(function() {
+    var speed = 300;
+    var href= $(this).attr("href");
+    var target = $(href == "#" || href == "" ? 'html' : href);
+    var position = target.offset().top;
+    $('body,html').animate({scrollTop:position}, speed, 'swing');
+    return false;
+  });
+});
